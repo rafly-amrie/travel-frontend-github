0e095477fe9ca6065fde61ee169705bc22120db3
Rafly Amrie <raflyyusrizalamrie@gmail.com>
2020-09-17 13:08:24 +0700

    Creating Button Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./assets/scss/style.scss"
+
+function App() {
+  return (
+    <div className="App" >
+      Hallo Obos
+    </div>
+  );
+}
+
+export default App;
---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import Button from './index'
+
+test('should not allowed to click the button if isDisabled is present', () => {
+    const {container} = render (<Button isDisabled></Button>)
+
+    expect(container.querySelector('span.disabled')).toBeInTheDocument()
+})
+
+test 
