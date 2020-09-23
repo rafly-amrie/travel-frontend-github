a86847f894d6ebf87f7d7c6c254955e1db69995e
Rafly Amrie <raflyyusrizalamrie@gmail.com>
2020-09-23 16:37:27 +0700

    Slicing Design

---
+++ slicing-travel-app/template/Header.php
@@ -0,0 +1,67 @@
+<? php >
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Apps</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="ui top fixed medium menu">
+
+        <div class="ui container">
+
+            <a class="active item">
+                Home
+            </a>
+
+            <a class="item">
+                Profile
+            </a>
+
+            <div class="ui dropdown item">
+                Project <i class="dropdown icon"></i>
+                <div class="menu">
+                    <a class="item">Start new project</a>
+                    <a class="item">View statistics</a>
+                    <a class="item">Manage</a>
+                </div>
+            </div>
+
+            <div class="ui dropdown item">
+                Courses <i class="dropdown icon"></i>
+                <div class="menu">
+                    <a class="item">Make course</a>
+                    <a class="item">Browse course-makers</a>
+                    <a class="item">Manage courses</a>
+                </div>
+            </div>
+
+
+            <div class="ui right dropdown item">
+                <img class="ui avatar image" src="http://www.acfe.com/uploadedImages/ACFE_Website/Content/images/membership-certification/graduate-member.jpg">
+                <div class="content">
+                    <div class="header">Daniel Louise</div>
+                </div>
+                <div class="menu">
+                    <a class="item">Profile</a>
+                    <a class="item">Dashboard</a>
+                    <a class="item">Manage courses</a>
+                </div>
+
+            </div>
+
+
+            <div class=" item">
+                <div class="ui primary button">Sign Up</div>
+            </div>
+
+        </div>
+
+    </div>
+<?>
